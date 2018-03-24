e2e103a8ef678ea9a9ffa6fa65db96968ac7da7e
Aman Tewary <aman.tewary@live.com>
2018-03-24 02:22:12 -0300

    Made some changes to the layout.

--- app/src/main/java/com/example/amant/cookingjournal/RecipeDetails.java
+++ app/src/main/java/com/example/amant/cookingjournal/RecipeDetails.java
@@ -2,9 +2,10 @@ package com.example.amant.cookingjournal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.SubtitleCollapsingToolbarLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,8 +18,10 @@ public class RecipeDetails extends AppCompatActivity {
     TextView recipeIngredients;
     TextView recipeSteps;
     TextView recipeUrl;
+    FloatingActionButton fab;
+    NestedScrollView menuSheet;
+    BottomSheetBehavior menuBehavior;
     MaterialRatingBar recipeRating;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +41,9 @@ public class RecipeDetails extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        menuSheet = findViewById(R.id.menu_sheet);
+        menuBehavior = BottomSheetBehavior.from(menuSheet);
+        fab = findViewById(R.id.fab2);
         recipeIngredients = findViewById(R.id.recipeDetailIngredients);
         recipeSteps = findViewById(R.id.recipeDetailSteps);
         recipeUrl = findViewById(R.id.recipeDetailLink);
@@ -57,13 +63,15 @@ public class RecipeDetails extends AppCompatActivity {
         recipeSteps.setText(steps);
         recipeUrl.setText(link);
         recipeRating.setRating(rating);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        menuBehavior.setPeekHeight(0);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(menuBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED){
+                    menuBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }else{
+                    menuBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
